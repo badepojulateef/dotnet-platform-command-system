166a2ced519a2ced032eaaec8bed178da4c8fdfd
Adepoju Lateef <badepojulateef@gmail.com>
2025-11-15 13:25:55 +0100

    added data layer - dbcontext

--- CommandService/obj/Debug/net8.0/CommandService.AssemblyInfo.cs
+++ CommandService/obj/Debug/net8.0/CommandService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4ba4c92cc9505fb675846f7a36c6f3cf65fc7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cba5792360d4f7f1637be24dcceef3667134cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
